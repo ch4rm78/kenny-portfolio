31ed1f3dcac38418d870b674d15a9a641209bc0a
Abiola-Ajayi Kehinde <kennyabiolaajayi@gmail.com>
2025-08-29 23:27:12 +0100

    new color

--- src/components/section/Home.jsx
+++ src/components/section/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           I’m passionate about solving real-world problems through technology and growing my expertise in full-stack development.
         </p>
         <div className="flex justify-center space-x-4">
-          <a href="#projects" className='bg-cyan-600 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden
+          <a href="#projects" className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden
                                         hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,139,246,0.4)]'>
             View Projects
           </a>
