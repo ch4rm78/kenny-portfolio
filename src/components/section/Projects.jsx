6fe50bf131c733b57db1ddbbf3e0310911c19d6c
Abiola-Ajayi Kehinde <kennyabiolaajayi@gmail.com>
2025-09-01 06:55:36 +0100

    descriptions modified

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -35,8 +35,9 @@ const Projects = () => {
           </div>
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
             <h3 className='text-xl font-bold mb-2'>Web Crawler</h3>
-            <p className='text-gray-400 mb-4'>A browser extension that detects and flags potential phishing websites in real time using LLMs, 
-              helping users browse safely and avoid online threats</p>
+            <p className='text-gray-400 mb-4'>A Python-powered web crawler designed to automate the process of scanning a list of URLs, 
+              extracting valid email addresses, and compiling them into a structured format. It helps save time and effort by streamlining 
+              data collection for outreach, research, or lead generation.</p>
             <div className='flex flex-wrap gap-2 mb-4'>
               {['Python', 'Flask', "Scrapy"].map((tech, key) => (
                 <span key={key} className="bg-blue-500/10 text-blue-500 px-2 py-1 rounded-full text-sm hover:bg-blue-500/20
@@ -55,8 +56,8 @@ const Projects = () => {
 
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
             <h3 className='text-xl font-bold mb-2'>Color Sorting Robotic Arm</h3>
-            <p className='text-gray-400 mb-4'>A browser extension that detects and flags potential phishing websites in real time using LLMs, 
-              helping users browse safely and avoid online threats</p>
+            <p className='text-gray-400 mb-4'>A robotic arm system powered by machine learning that identifies and sorts items based on their colors. Using sensors and a camera for detection, 
+              the arm automatically classifies objects and places them into the correct containers, demonstrating intelligent automation and efficient object handling.</p>
             <div className='flex flex-wrap gap-2 mb-4'>
               {['Python', 'Arduino Uno', "OpenCV", "C++"].map((tech, key) => (
                 <span key={key} className="bg-blue-500/10 text-blue-500 px-2 py-1 rounded-full text-sm hover:bg-blue-500/20
@@ -70,6 +71,24 @@ const Projects = () => {
               <a href="https://github.com/ch4rm78/3dof-Robotic-arm-to-sort-items-based-on-color">
                 <FaGithub className='w-10 h-10 mt-3'/>
               </a>
+            </div>
+          </div>
+
+           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <h3 className='text-xl font-bold mb-2'>Job Seekers Platform</h3>
+            <p className='text-gray-400 mb-4'>A job seekers platform that connects candidates with potential employers, 
+              making it easier to discover opportunities and build meaningful career connections.</p>
+            <div className='flex flex-wrap gap-2 mb-4'>
+              {['React Js', 'Supabase', "Tailwind", "Node.js"].map((tech, key) => (
+                <span key={key} className="bg-blue-500/10 text-blue-500 px-2 py-1 rounded-full text-sm hover:bg-blue-500/20
+                                                            hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <div className='flex justify-between items-center'>
+              <h3>In Progress...</h3>
             </div>
           </div>
 
